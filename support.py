7fb75b7b7cac1e19997d8b870ce45f2cb79d0f1b
wgrigor <wgrigor21@nwphs.org>
2019-05-04 16:44:13 -0500

    Update to JSON based simulation configuration

--- support.py
+++ support.py
@@ -1,10 +1,15 @@
 import numpy as np
+import json
 
 def loadData(path):
     try:
-        data = np.loadtxt(path, delimiter=',', skiprows=1, unpack=True)
-        print(data)
+        if path[-5:] != ".json":
+            raise ValueError
+        data = json.loads(open(path, 'r').read())
         return data
     except OSError:
         print("File not found!")
+        return None
+    except ValueError:
+        print("File extension invalid, are you loading a JSON file?")
         return None
